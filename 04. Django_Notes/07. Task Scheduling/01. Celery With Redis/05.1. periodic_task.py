a313633415a9f4b9bcaf23e9f65d89be29d042ac
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-08-15 04:58:55 +0600

    Update 05.1. periodic_task.py

--- 04. Django_Notes/07. Task Scheduling/01. Celery With Redis/05.1. periodic_task.py
+++ 04. Django_Notes/07. Task Scheduling/01. Celery With Redis/05.1. periodic_task.py
@@ -1,17 +1,27 @@
-# task.py
+# app/task.py
 from celery import shared_task
-
 @shared_task
 def my_periodic_task():
     # Your periodic task logic here
     print("Executing periodic task...")
+
 
 # celery.py
 from celery.schedules import crontab
 
 CELERY_BEAT_SCHEDULE = {
     'my-periodic-task': {
-        'task': 'path.to.your.task.my_periodic_task',
+        'task': 'app_name.file_name.my_periodic_task',
         'schedule': crontab(minute='*/15'),  # Execute every 15 minutes
+    },
+    'add-every-30-seconds': {
+        'task': 'app1.tasks.add',
+        'schedule': 30.0,
+        'args': (16, 16)
+    },
+    'multiply-at-noon': {
+        'task': 'app2.tasks.multiply',
+        'schedule': crontab(hour=12, minute=0),
+        'args': (4, 5)
     },
 }
