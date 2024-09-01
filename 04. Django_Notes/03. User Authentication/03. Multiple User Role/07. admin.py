7853b165240d112507b3da20b1461f4e5537bf36
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-09-02 01:55:28 +0600

    Update 07. admin.py

--- 04. Django_Notes/03. User Authentication/03. Multiple User Role/07. admin.py
+++ 04. Django_Notes/03. User Authentication/03. Multiple User Role/07. admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import AppUser, Teacher, Student
 
 admin.site.register(AppUser)
-admin.site.register(Student)
+admin.site.register(Teacher)
 admin.site.register(Student)
 admin.site.site_header = 'AI Writing Project'   
 
