76e57f7f04a3c4e51d8723f6ae11d6a7e906f9e6
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-09-02 04:17:32 +0600

    Update 02. setting.py

--- 04. Django_Notes/02. setting.py
+++ 04. Django_Notes/02. setting.py
@@ -19,14 +19,15 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles" # for collect static
 
+'''
 # This is for Hosted server and When Debug is False after Hosting Django App in Domain
-# >>> python manage.py collectstatic
-# >>> Now configure Project urls.py
-# >>> Or collect CSS and from `staticfiles` folder and paste it in `static` folder
-# >>> Turn off/on Debug True/False and Restart app It will fix
-# >>> If Debug is True then admin CSS won't work in the local Server
-# >>> 404.html and Debug is false for 404 page
-
+>>> python manage.py collectstatic
+- Now configure Project urls.py
+- Or collect CSS and from `staticfiles` folder and paste it in `static` folder
+- Turn off/on Debug True/False and Restart app It will fix
+- If Debug is True then admin CSS won't work in the local Server
+- 404.html and Debug is false for 404 page
+'''
 
 # For media support, also need to create ` media  ` in media >>> ` images ` folder where have manage.py 
 # Also, need to Configure urls.py file for ` media support ` 
@@ -51,7 +52,6 @@ DATABASES = {
 # logs / console.log  
 
 import os
-from pathlib import Path
 from django.conf import settings
 
 LOGGING = {
