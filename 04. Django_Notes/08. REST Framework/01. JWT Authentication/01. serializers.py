0cfa7446d69436583a6b30511263a838e60584e1
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-09-22 02:36:17 +0600

    Update 01. serializers.py

--- 04. Django_Notes/08. REST Framework/01. JWT Authentication/01. serializers.py
+++ 04. Django_Notes/08. REST Framework/01. JWT Authentication/01. serializers.py
@@ -3,7 +3,12 @@ from rest_framework import serializers
 
 
 
-# Serializer for making post request
+# Serializer without Django Models
+class GenerateTextSerializer(serializers.Serializer):
+    input_text = serializers.CharField(max_length=1000
+
+
+# Serializer for making post request to retrive data from django Model
 class DataListSerializers(serializers.ModelSerializer):
     website_name = serializers.CharField(source='login_site.website_name', read_only=True)  # Data from Foreign Model
     class Meta:
@@ -12,3 +17,4 @@ class DataListSerializers(serializers.ModelSerializer):
         fields = ['id', 'website_name', 'login_link', 'username', 'password']  
         # makesure same field name, website_name comes from Foreign Model, 
         # otherwise we can use ` login_site ` but it will return Foreign model ` number `
+
