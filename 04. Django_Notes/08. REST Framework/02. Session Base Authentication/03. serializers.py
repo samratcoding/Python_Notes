da3fe09add7ad6abd8066885b05d96ba0db77280
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-09-22 02:42:33 +0600

    Update 03. serializers.py

--- 04. Django_Notes/08. REST Framework/02. Session Base Authentication/03. serializers.py
+++ 04. Django_Notes/08. REST Framework/02. Session Base Authentication/03. serializers.py
@@ -4,17 +4,8 @@
 
 from rest_framework import serializers
 
-class GenerateTextSerializer(serializers.Serializer):
-    input_text = serializers.CharField(max_length=1000)
-
-
-class Info_Bulk_Posting_Serializer(serializers.Serializer):
-    keyword_list = serializers.CharField()
-    website_id = serializers.CharField(max_length=500)
-    youtubeapi_id = serializers.CharField(max_length=500, required=False, allow_blank=True)
-    category = serializers.CharField(max_length=500, required=False, allow_blank=True)
-    post_status = serializers.CharField(max_length=200)
-    img_status = serializers.CharField(max_length=200)
-
+class TextSerializer(serializers.Serializer):
+    input_text1 = serializers.CharField(max_length=1000)
+    input_text2 = serializers.CharField(max_length=1000)
 
 # ----- After any change or add serializer field need to do database migrations -----
