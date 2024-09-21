ead30f1b7a0691c803cb6de98022e7ada5f7ddb0
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-09-22 02:45:01 +0600

    Update 04. views.py

--- 04. Django_Notes/08. REST Framework/02. Session Base Authentication/04. views.py
+++ 04. Django_Notes/08. REST Framework/02. Session Base Authentication/04. views.py
@@ -4,14 +4,15 @@ from rest_framework import status
 from .serializers import GenerateTextSerializer
 
 
-class GenerateTextView(APIView):
+class DataView(APIView):
     def post(self, request):
         print(request.user)
         if request.user.is_authenticated:
             serializer = GenerateTextSerializer(data=request.data)
             if serializer.is_valid():
-                input_text = serializer.validated_data['input_text']
-                generated_text = f"Generated text for '{input_text}'"
+                input_text1 = serializer.validated_data['input_text1']
+                input_text2 = serializer.validated_data['input_text2']
+                generated_text = f"{input_text1} + {input_text2}"
                 return Response({'generated_text': generated_text}, status=status.HTTP_200_OK) # This status will pass response.ok
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # This status will pass !response.ok/ not ok
 
