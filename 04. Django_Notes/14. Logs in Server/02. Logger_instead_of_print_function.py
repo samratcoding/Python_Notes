2bfd7fad6a9dae9bb484a50935f4a0a8cda99ffa
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-09-25 04:36:35 +0600

    Update 02. Logger_instead_of_print_function.py

--- 04. Django_Notes/14. Logs in Server/02. Logger_instead_of_print_function.py
+++ 04. Django_Notes/14. Logs in Server/02. Logger_instead_of_print_function.py
@@ -7,7 +7,7 @@ logger = logging.getLogger("django")
 def your_view_function(request):
     name = 'Samrat'
     age = 25
-    logger.info('This is bulk info posting page, testing for Python logger')
-    logger.info(f' This message in from logger, name: {name} and  age :{str(age)}')
+    logger.info('Appname.filename.py >>> This is bulk info posting page, testing for Python logger')
+    logger.info(f'Appname.filename.py >>> This message in from logger, name: {name} and  age :{str(age)}')
     # Logger can take one argument not like print() function
     return render(request, template, context=context)
