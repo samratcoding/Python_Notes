c211c16b085090cee26adb6b3b03dd7203c160dd
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-09-28 14:48:05 +0600

    Update admin.py

--- 04. Django_Notes/05. Models with CRUD/admin.py
+++ 04. Django_Notes/05. Models with CRUD/admin.py
@@ -21,7 +21,8 @@ from .models import ModelName
 class CustomField(admin.ModelAdmin):
     list_display = ('field_name1', 'field_name2')   # Fields to show in list view
     list_filter = ('field_name1', 'field_name2')    # Optional: Add filters to filter by these fields
-    search_fields = ('field_name1', 'field_name2')  # For search filter
+    search_fields = ('field_name1', 'field_name2', 'user__email')  # For search filter
+    ''' field_name__related_field_name, user__email   : here uses  __ for Forigen Model field'''
     fieldsets = (
         ("Section Name", {
             'fields': ('field_name1', 'field_name2'),
