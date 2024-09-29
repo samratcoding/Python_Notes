b2f8fb71148a5bffee3555e445e672f147de8a8c
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-09-30 03:07:32 +0600

    Update 08. multithreding_practice.py

--- 01. Fundamental/08. multithreding_practice.py
+++ 01. Fundamental/08. multithreding_practice.py
@@ -1,27 +1,20 @@
-from time import sleep
 import threading
 
-def fun1(number):
-    for num in number:
-        sleep(0.5)
-        print('Fun 1 : ', num)
-def fun2(number):
-    for num in number:
-        sleep(0.5)
-        print('Fun 2 : ', num)
+def thread_task(name):
+    print(f"Task {name} is running")
 
-
-# Multi Threading
-lst = [1,2,3,4,5,6,7,8,9]
-
-t1 = threading.Thread(target=fun1, args=(lst,))
-t2 = threading.Thread(target=fun2, args=(lst,))
-
-t1.start()
-# t1.join()  this join hold this threding
-
-t2.start()
-# t12.join()  this join hold this threding
-
-t1.join()
-t2.join()
+task = 10
+i = 0
+max_thread = 5
+thread_gather = []
+while i < task
+    for i in range(max_thread):
+        if i < task:
+            thread = threading.Thread(target=thread_task, args=(i,))
+            thread_gather.append(thread)
+            thread_gather.start()
+        else:
+            print('All Task Appended')
+            
+for thread in thread_gather:
+    thread.join()
