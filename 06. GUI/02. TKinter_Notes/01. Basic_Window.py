ee36e834a3c2966396f4594acd1643ad2a40db4e
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-10-02 02:57:36 +0600

    Update 01. Basic_Window.py

--- 06. GUI/02. TKinter_Notes/01. Basic_Window.py
+++ 06. GUI/02. TKinter_Notes/01. Basic_Window.py
@@ -8,7 +8,13 @@ window.geometry("900x600")   # Window Area
 frame = Frame(window) # Main Frame
 frame.pack()
 
-
+from datetime import datetime
+target_date = datetime(2024, 10, 10)
+current_date = datetime.now()
+if current_date <= target_date:
+    print("ok")
+else:
+    print("expired")
 
 
 if __name__ == '__main__':
